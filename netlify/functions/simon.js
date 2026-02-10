6929800a5cf43dedae612b0c060ffe928f400116
sdierct-cyber <sdierct@gmail.com>
2026-02-10 01:46:16 -0500

    Update simon.js

--- netlify/functions/simon.js
+++ netlify/functions/simon.js
@@ -328,3 +328,11 @@ Style:
   return base + "\n\nMode: CHAT. Be human, concise, and useful.";
 }
 
+function hash(str) {
+  let h = 0;
+  for (let i = 0; i < str.length; i++) {
+    h = (h << 5) - h + str.charCodeAt(i);
+    h |= 0;
+  }
+  return h;
+}
